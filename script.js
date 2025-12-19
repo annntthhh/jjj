381dbc7251ccf7fe838fa3a28f2b2ac697559d50
annntthhh <anethblanco2010@gmail.com>
2025-12-19 14:48:43 -0400

    script.js

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
+// Lógica del Reloj (Idea 3)
+let minutos = 90;
+let segundos = 0;
+const reloj = document.getElementById('reloj-partido');
+
+setInterval(() => {
+    segundos++;
+    if (segundos === 60) {
+        minutos++;
+        segundos = 0;
+    }
+    reloj.innerText = `${minutos}:${segundos < 10 ? '0' + segundos : segundos}+`;
+}, 1000);
+
+// Lógica del Botón y sorpresas
 const boton = document.getElementById('btnInteractivo');
 const resultado = document.getElementById('resultado');
 const fondoRayado = document.getElementById('bg');
 
 boton.addEventListener('click', function() {
-    // 1. Mostrar el resultado
+    // Mostrar el resultado
     resultado.classList.remove('hidden');
     
-    // 2. Esconder el botón
+    // Esconder el botón
     boton.style.display = 'none';
     
-    // 3. Efecto de victoria en el fondo
-    document.body.style.backgroundColor = "#edbb00"; // Color Dorado
-    fondoRayado.style.opacity = "0.1"; // Atenuar las rayas
+    // Efecto de victoria en el fondo
+    document.body.style.backgroundColor = "#edbb00"; 
+    fondoRayado.style.opacity = "0.1"; 
     
-    // 4. Mensaje de alerta (Sorpresa)
+    // Mensaje de alerta
     setTimeout(() => {
         alert("¡CAMPEONES DE NUESTRO AMOR! ❤️💙");
     }, 500);
