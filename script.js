688e1e21fdb51919d77c43b39e4e8f8d7491e661
annntthhh <anethblanco2010@gmail.com>
2025-12-19 14:53:06 -0400

    script.js

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-// Lógica del Reloj (Idea 3)
+// Reloj
 let minutos = 90;
 let segundos = 0;
 const reloj = document.getElementById('reloj-partido');
 
 setInterval(() => {
     segundos++;
-    if (segundos === 60) {
-        minutos++;
-        segundos = 0;
-    }
+    if (segundos === 60) { minutos++; segundos = 0; }
     reloj.innerText = `${minutos}:${segundos < 10 ? '0' + segundos : segundos}+`;
 }, 1000);
 
-// Lógica del Botón y sorpresas
+// Botón Principal
 const boton = document.getElementById('btnInteractivo');
 const resultado = document.getElementById('resultado');
 const fondoRayado = document.getElementById('bg');
 
 boton.addEventListener('click', function() {
-    // Mostrar el resultado
     resultado.classList.remove('hidden');
-    
-    // Esconder el botón
     boton.style.display = 'none';
-    
-    // Efecto de victoria en el fondo
     document.body.style.backgroundColor = "#edbb00"; 
     fondoRayado.style.opacity = "0.1"; 
-    
-    // Mensaje de alerta
+    setTimeout(() => { alert("¡CAMPEONES DE NUESTRO AMOR! ❤️💙"); }, 500);
+});
+
+// Lógica del VAR (Idea 1)
+const btnVar = document.getElementById('btnVar');
+const varMensaje = document.getElementById('varMensaje');
+
+btnVar.addEventListener('click', function() {
+    btnVar.innerText = "REVISANDO...";
     setTimeout(() => {
-        alert("¡CAMPEONES DE NUESTRO AMOR! ❤️💙");
-    }, 500);
+        btnVar.style.display = 'none';
+        varMensaje.classList.remove('hidden');
+    }, 1500);
 });
